e2d3b5bb2e408995a24c6e485a90085162b573ea
iJIAJIA <503690080@qq.com>
2018-02-24 17:56:59 +0800

    ehcache

---
+++ demo/src/test/java/ehcache/ProgrammaticConfiguration.java
@@ -0,0 +1,46 @@
+package ehcache;
+
+import java.text.MessageFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.junit.Test;
+
+public class ProgrammaticConfiguration {
+
+	public static void main(String[] args) {
+		CacheManager cacheManager = CacheManagerBuilder
+				.newCacheManagerBuilder()
+				.withCache(
+						"preConfigured",
+						CacheConfigurationBuilder.newCacheConfigurationBuilder(
+								Long.class, String.class,
+								ResourcePoolsBuilder.heap(10))).build();
+		cacheManager.init();
+		Cache<Long, String> myCache = cacheManager
+				.createCache("myCache", CacheConfigurationBuilder
+						.newCacheConfigurationBuilder(Long.class, String.class,
+								ResourcePoolsBuilder.heap(10)));
+
+		Cache<Long, String> preConfigured = cacheManager.getCache(
+				"preConfigured", Long.class, String.class);
+		
+		System.out.println(preConfigured.containsKey(1L));
+		
+
+		myCache.put(1L, "da one!");
+		String value = myCache.get(1L);
+		System.out.println(value);
+		cacheManager.removeCache("preConfigured");
+
+		cacheManager.close();
+	}
+	
+}
