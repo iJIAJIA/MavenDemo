a076a5ed7d95546c850c16fe5b3871d39bc1daf4
zhoujiaquan <zhoujiaquan@peilian.com>
2019-03-12 21:52:51 +0800

    时区计算

---
+++ demo/src/test/java/timezone/TimeZoneOffsetTest.java
@@ -0,0 +1,43 @@
+package timezone;
+
+import java.time.ZoneOffset;
+import java.util.TimeZone;
+
+import org.junit.Test;
+
+public class TimeZoneOffsetTest {
+	
+	/**
+	 * ZoneOffset的描述： A time-zone offset is the amount of time that a time-zone differs from Greenwich/UTC. </br>
+	 * <p>ZoneOffset是 <b>当地时间-格林尼治</b>的时间差</p> </br>
+	 * javascript的 new Date().getTimezoneOffset(); 是 <b>格林尼治-当地时间</b>的 时间差，精确到分钟。</br>
+	 * <p>从前端获取TimezoneOffset时需要进行<b>正负切换</b></p>
+	 */
+	@Test
+	public void test() {
+		TimeZone timeZone = TimeZone.getTimeZone(ZoneOffset.ofHoursMinutes(8, 00));
+		TimeZone timeZone2 = TimeZone.getTimeZone("Asia/Shanghai");
+		System.out.printf("timezoneFromOffset:%d,timezoneFromZoneId:%d",timeZone.getRawOffset(),timeZone2.getRawOffset());
+		
+	}
+	
+	/**
+	 * 计算当地零点时间戳
+	 * unix时间戳 是格林威治时间1970年01月01日00时00分00秒(北京时间1970年01月01日08时00分00秒)起至现在的总秒数
+	 * 
+	 */
+	@Test
+	public void test2() {
+		long current = System.currentTimeMillis();
+		// 前提：unix时间戳 是格林威治时间1970年01月01日00时00分00秒起至现在的总秒数
+		// 每个地方计算unix时间戳的起始时间不一样 北京计算起始时间为1970年01月01日08时00分00秒
+		// 以天为周期1000*3600*24， current/1000*3600*24 获取至今的天数
+		// result以天为单位运算得出的整天数时间戳，此时间戳为格林尼治的零点时间戳
+		// 因为时区问题，在北京此时的result时间戳对应北京的早上8点
+		// 因此需要执行result-timezoneOffset 减掉时区偏移量，从而得出北京的零点时间戳
+		long result = current/(1000*3600*24)*(1000*3600*24);
+		long timezoneOffset = TimeZone.getDefault().getRawOffset();
+		System.out.printf("result:%d,offset:%d,sum:%d",result,timezoneOffset,result-timezoneOffset);
+	}
+	
+}
